17b90436614d932fae215ebec7b9737d170be31a
joon-hyuk01 <139195177+joon-hyuk01@users.noreply.github.com>
2024-04-01 23:27:38 +0900

    Update pcap.c

--- pcap.c
+++ pcap.c
@@ -12,18 +12,18 @@ struct ethheader {
 
 /* IP Header */
 struct ipheader {
-  unsigned char      iph_ihl:4,   //IP header length
-                     iph_ver:4;    //IP version
-  unsigned char      iph_tos;      //Type of service
-  unsigned short int iph_len;      //IP Packet length (data + header)
-  unsigned short int iph_ident;    //Identification
-  unsigned short int iph_flag:3,   //Fragmentation flags
-                     iph_offset:13; //Flags offset
-  unsigned char      iph_ttl;      //Time to Live
-  unsigned char      iph_protocol; //Protocol type
-  unsigned short int iph_chksum;   //IP datagram checksum
-  struct  in_addr    iph_sourceip; //Source IP address
-  struct  in_addr    iph_destip;   //Destination IP address
+    unsigned char      iph_ihl:4,   //IP header length
+                       iph_ver:4;    //IP version
+    unsigned char      iph_tos;      //Type of service
+    unsigned short int iph_len;      //IP Packet length (data + header)
+    unsigned short int iph_ident;    //Identification
+    unsigned short int iph_flag:3,   //Fragmentation flags
+                       iph_offset:13; //Flags offset
+    unsigned char      iph_ttl;      //Time to Live
+    unsigned char      iph_protocol; //Protocol type
+    unsigned short int iph_chksum;   //IP datagram checksum
+    struct  in_addr    iph_sourceip; //Source IP address
+    struct  in_addr    iph_destip;   //Destination IP address
 };
 
 /* TCP Header */
@@ -74,7 +74,15 @@ void packet_capture(u_char *args, const struct pcap_pkthdr *header, const u_char
     printf("Source Port: %d\n", ntohs(tcp->tcp_sport));
     printf("Destination Port: %d\n", ntohs(tcp->tcp_dport));
 
-    printf("\n");
+    // TCP 메시지 출력 (첫 8바이트만 출력)
+    printf("Message: ");
+    int message_len = ntohs(ip->iph_len) - ip_header_length - (tcp->tcp_offx2 >> 4) * 4;
+    int print_len = message_len > 8 ? 8 : message_len;
+    for (int i = 0; i < print_len; i++) {
+        printf("%02x ", packet[ip_header_length + (tcp->tcp_offx2 >> 2) + i]);
+    }
+
+    printf("\n\n");
 }
 
 int main(){
@@ -92,4 +100,3 @@ int main(){
 
     return 0;
 }
-
